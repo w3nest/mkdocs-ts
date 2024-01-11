f55b6360776cd98e2076a599c15bdd7fa4f3bf6e
youwol-guillaume <greinisch@youwol.com>
2024-01-11 10:47:29 +0100

    🎉 generated from template
    Used the `pipeline-ts-webpack-npm` of py-youwol

---
+++ src/auto-generated.ts
@@ -0,0 +1,117 @@
+
+const runTimeDependencies = {
+    "externals": {
+        "@youwol/rx-vdom": "^1.0.1",
+        "rxjs": "^7.5.6"
+    },
+    "includedInBundle": {}
+}
+const externals = {
+    "@youwol/rx-vdom": {
+        "commonjs": "@youwol/rx-vdom",
+        "commonjs2": "@youwol/rx-vdom",
+        "root": "@youwol/rx-vdom_APIv1"
+    },
+    "rxjs": {
+        "commonjs": "rxjs",
+        "commonjs2": "rxjs",
+        "root": "rxjs_APIv7"
+    }
+}
+const exportedSymbols = {
+    "@youwol/rx-vdom": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/rx-vdom"
+    },
+    "rxjs": {
+        "apiKey": "7",
+        "exportedSymbol": "rxjs"
+    }
+}
+
+const mainEntry : {entryFile: string,loadDependencies:string[]} = {
+    "entryFile": "./lib/index.ts",
+    "loadDependencies": [
+        "@youwol/rx-vdom",
+        "rxjs"
+    ]
+}
+
+const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {}
+
+const entries = {
+     '@youwol/mkdocs-ts': './lib/index.ts',
+    ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/mkdocs-ts/${e.name}`]:e.entryFile}), {})
+}
+export const setup = {
+    name:'@youwol/mkdocs-ts',
+        assetId:'QHlvdXdvbC9ta2RvY3MtdHM=',
+    version:'0.1.0-wip',
+    shortDescription:"",
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/mkdocs-ts&tab=doc',
+    npmPackage:'https://www.npmjs.com/package/@youwol/mkdocs-ts',
+    sourceGithub:'https://github.com/youwol/mkdocs-ts',
+    userGuide:'https://l.youwol.com/doc/@youwol/mkdocs-ts',
+    apiVersion:'01',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+    secondaryEntries,
+    getDependencySymbolExported: (module:string) => {
+        return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    },
+
+    installMainModule: ({cdnClient, installParameters}:{
+        cdnClient:{install:(unknown) => Promise<WindowOrWorkerGlobalScope>},
+        installParameters?
+    }) => {
+        const parameters = installParameters || {}
+        const scripts = parameters.scripts || []
+        const modules = [
+            ...(parameters.modules || []),
+            ...mainEntry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/mkdocs-ts_APIv01`]
+        })
+    },
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{
+        name: string,
+        cdnClient:{install:(unknown) => Promise<WindowOrWorkerGlobalScope>},
+        installParameters?
+    }) => {
+        const entry = secondaryEntries[name]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const parameters = installParameters || {}
+        const scripts = [
+            ...(parameters.scripts || []),
+            `@youwol/mkdocs-ts#0.1.0-wip~dist/@youwol/mkdocs-ts/${entry.name}.js`
+        ]
+        const modules = [
+            ...(parameters.modules || []),
+            ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/mkdocs-ts/${entry.name}_APIv01`]
+        })
+    },
+    getCdnDependencies(name?: string){
+        if(name && !secondaryEntries[name]){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const deps = name ? secondaryEntries[name].loadDependencies : mainEntry.loadDependencies
+
+        return deps.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+    }
+}
