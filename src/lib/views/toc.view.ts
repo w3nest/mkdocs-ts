643da9bc9d7261bbbede51fb5b12e74127c73541
youwol-guillaume <greinisch@youwol.com>
2024-01-15 08:55:42 +0100

    ✨ `TOCView` => reactive style w/ `onscroll`

--- src/lib/views/toc.view.ts
+++ src/lib/views/toc.view.ts
@@ -1,5 +1,6 @@
 import { AnyVirtualDOM, ChildrenLike, VirtualDOM } from '@youwol/rx-vdom'
 import { Router } from '../router'
+import { BehaviorSubject } from 'rxjs'
 
 export class TOCView implements VirtualDOM<'div'> {
     public readonly router: Router
@@ -11,13 +12,16 @@ export class TOCView implements VirtualDOM<'div'> {
         fontSize: '0.9rem',
         lineHeight: '1.5rem',
     }
+
+    public readonly indexFirstVisibleHeading$ = new BehaviorSubject<number>(0)
     constructor(params: {
         html: HTMLElement
         router: Router
         domConvertor?: (e: HTMLHeadingElement) => AnyVirtualDOM
     }) {
         Object.assign(this, params)
-        const headings = this.html.querySelectorAll('h1, h2, h3')
+        const headings: NodeListOf<HTMLElement> =
+            this.html.querySelectorAll('h1, h2, h3, h4')
         const defaultConv = (heading: HTMLElement) => ({
             tag: 'div' as const,
             class: 'fv-hover-text-focus',
@@ -32,6 +36,10 @@ export class TOCView implements VirtualDOM<'div'> {
             H2: '1em',
             H3: '2em',
         }
+        this.router.scrollableElement.onscroll = () => {
+            this.getFirstVisible(headingsArray)
+        }
+
         this.children = [
             headingsArray.length > 0
                 ? {
@@ -45,39 +53,60 @@ export class TOCView implements VirtualDOM<'div'> {
             {
                 tag: 'ul',
                 class: 'p-0',
-                children: headingsArray.map((heading: HTMLHeadingElement) => {
-                    return {
-                        tag: 'li' as const,
-                        class: heading.classList.value,
-                        style: { paddingLeft: padding[heading.tagName] },
-                        children: [
-                            {
-                                tag: 'a' as const,
-                                class: 'fv-hover-text-focus',
-                                style: {
-                                    color: 'black',
-                                    textDecoration: 'none',
+                children: headingsArray.map(
+                    (heading: HTMLHeadingElement, index: number) => {
+                        const getItemClass = (firstIndex: number) => {
+                            if (index == firstIndex) {
+                                return 'fv-text-focus font-weight-bold'
+                            }
+                            return index < firstIndex
+                                ? 'text-dark'
+                                : 'fv-text-disabled'
+                        }
+                        return {
+                            tag: 'li' as const,
+                            class: heading.classList.value,
+                            style: { paddingLeft: padding[heading.tagName] },
+                            children: [
+                                {
+                                    tag: 'a' as const,
+                                    class: {
+                                        source$: this.indexFirstVisibleHeading$,
+                                        vdomMap: getItemClass,
+                                        wrapper: (d) =>
+                                            `fv-hover-text-focus ${d} `,
+                                    },
+                                    href: `${
+                                        this.router.basePath
+                                    }?nav=${this.router.getCurrentPath()}.${
+                                        heading.id
+                                    }`,
+                                    children: [
+                                        (params.domConvertor || defaultConv)(
+                                            heading,
+                                        ),
+                                    ],
+                                    onclick: (ev) => {
+                                        ev.preventDefault()
+                                        this.router.scrollTo(heading)
+                                    },
                                 },
-                                href: `${
-                                    this.router.basePath
-                                }?nav=${this.router.getCurrentPath()}.${
-                                    heading.id
-                                }`,
-                                children: [
-                                    (params.domConvertor || defaultConv)(
-                                        heading,
-                                    ),
-                                ],
-                                onclick: (ev) => {
-                                    ev.preventDefault()
-                                    this.router.scrollTo(heading)
-                                },
-                            },
-                        ],
-                    }
-                }),
+                            ],
+                        }
+                    },
+                ),
             },
         ]
+    }
+    getFirstVisible(headings: HTMLElement[]) {
+        for (let i = 0; i < headings.length; i++) {
+            const rect = headings[i].getBoundingClientRect()
+
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                this.indexFirstVisibleHeading$.next(i)
+                return
+            }
+        }
     }
 }
 
