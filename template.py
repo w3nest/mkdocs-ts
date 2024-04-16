1a259c3f6cdff115e892093ee8de7ddee37c63c5
youwol-guillaume <greinisch@youwol.com>
2024-04-16 17:12:22 +0200

    🔧 configure auxiliary module `CodeApi`

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import shutil
 from pathlib import Path
 
 from youwol.pipelines.pipeline_typescript_weback_npm import Template, PackageType, Dependencies, \
-    RunTimeDeps, Bundles, MainModule
+    RunTimeDeps, Bundles, MainModule, AuxiliaryModule
 from youwol.pipelines.pipeline_typescript_weback_npm.regular import generate_template
 from youwol.utils import parse_json
 
@@ -17,7 +17,8 @@ externals_deps = {
     "marked": "^4.2.3",
     "highlight.js": "11.2.0",
     "@youwol/os-top-banner": "^0.2.0",
-    "@youwol/rx-tree-views": "^0.3.3"
+    "@youwol/rx-tree-views": "^0.3.3",
+    "@youwol/http-primitives": "^0.2.3",
 }
 in_bundle_deps = {}
 dev_deps = {}
@@ -41,7 +42,14 @@ template = Template(
             entryFile='./index.ts',
             loadDependencies=list(externals_deps.keys()),
             aliases=[]
-        )
+        ),
+        auxiliaryModules=[
+            AuxiliaryModule(
+                name='CodeApi',
+                entryFile='./lib/code-api/index.ts',
+                loadDependencies=["@youwol/rx-vdom", "@youwol/http-primitives" ],
+            )
+        ]
     ),
     userGuide=True
 )
@@ -52,7 +60,8 @@ shutil.copyfile(
     dst=folder_path / 'src' / 'auto-generated.ts'
 )
 for file in ['README.md', '.gitignore', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
-             'tsconfig.json', 'jest.config.ts', 'webpack.config.ts']:
+             # 'tsconfig.json', add `"exclude": ["mkdocs-ts-doc"]`
+             'jest.config.ts', 'webpack.config.ts']:
     shutil.copyfile(
         src=folder_path / '.template' / file,
         dst=folder_path / file
--- typedoc.js
+++ typedoc.js
@@ -1,6 +1,6 @@
 /* eslint-env node -- eslint-comment add exception because the running context is node environment */
 module.exports = {
-    entryPoints: ['./src/index.ts'],
+    entryPoints: ['./src/index.ts', './src/lib/code-api/index.ts'],
     exclude: ['src/tests'],
     out: 'dist/docs',
     theme: 'default',
