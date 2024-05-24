debc7fe49cd77e579abb2836f720436081c17ed2
youwol-guillaume <greinisch@youwol.com>
2024-05-24 12:18:09 +0200

    📝 [doc-app] Minor updates

--- mkdocs-ts-doc/rx-vdom-config.ts
+++ mkdocs-ts-doc/rx-vdom-config.ts
@@ -1,8 +1,8 @@
+type Mode = 'Prod'
 type AllTags = keyof HTMLElementTagNameMap
 export type Configuration = {
     TypeCheck: 'strict'
-    SupportedHTMLTags: 'Prod' extends 'Prod' ? AllTags : DevTags
-    WithFluxView: false
+    SupportedHTMLTags: Mode extends 'Prod' ? AllTags : DevTags
 }
 
 type DevTags = 'div'
