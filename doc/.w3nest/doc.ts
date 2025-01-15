ec4ec7c560bbe285b3d16615783ff02a8eb951dd
youwol-guillaume <greinisch@youwol.com>
2025-01-15 17:38:23 +0100

    🔧 [doc-app] Link `rx-vdom` & `rxjs` symbols in API doc.

--- doc/.w3nest/doc.ts
+++ doc/.w3nest/doc.ts
@@ -4,4 +4,23 @@ generateApiFiles({
     projectFolder: `${__dirname}/../../`,
     outputFolder: `${__dirname}/../assets/api`,
     baseNav: '/api',
+    externals: {
+        'rx-vdom': ({ name }: { name: string }) => {
+            return `/apps/@rx-vdom/doc/latest?nav=/api.${name}`
+        },
+        rxjs: ({ name }: { name: string }) => {
+            const urls = {
+                Subject: 'https://www.learnrxjs.io/learn-rxjs/subjects/subject',
+                BehaviorSubject:
+                    'https://www.learnrxjs.io/learn-rxjs/subjects/subject',
+                ReplaySubject:
+                    'https://www.learnrxjs.io/learn-rxjs/subjects/replaysubject',
+                Observable: 'https://rxjs.dev/guide/observable',
+            }
+            if (!(name in urls)) {
+                console.warn(`Can not find URL for rxjs ${name} symbol`)
+            }
+            return urls[name]
+        },
+    },
 })
