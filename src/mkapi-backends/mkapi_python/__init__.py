78085ecc27ab7ea49d74de4fa2d25214c845ed0f
GReinisch <reinisch.gui@gmail.com>
2025-02-17 13:39:28 +0100

    🔧 [mkapi_py] Define project using `pyproject.toml`

--- src/mkapi-backends/mkapi_python/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This module defines helping utilities for generating documentation models that can be consumed from
-front-ends to provide interactive API documentation.
-
-"""
-
-from .models import *
-# relative
-from .py_griffe import *
