75c078466ef4f6360c3be766e65aabb5fc2240d1
GReinisch <reinisch.gui@gmail.com>
2025-03-10 10:27:07 +0100

    📝 [doc-app] Expose `typescript` symbols link

--- doc/.w3nest/doc.ts
+++ doc/.w3nest/doc.ts
@@ -8,6 +8,25 @@ generateApiFiles({
         'rx-vdom': ({ name }: { name: string }) => {
             return `/apps/@rx-vdom/doc/latest?nav=/api.${name}`
         },
+        typescript: ({ name }: { name: string }) => {
+            const urls = {
+                Promise: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise',
+                HTMLElement: 'https://www.typescriptlang.org/docs/handbook/dom-manipulation.html',
+                Record: 'https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type',
+                Pick: 'https://www.typescriptlang.org/docs/handbook/utility-types.html#picktype-keys',
+                MouseEvent: 'https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent',
+                Partial: 'https://www.typescriptlang.org/docs/handbook/utility-types.html#partialtype',
+                Omit: 'https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys',
+                window: 'https://developer.mozilla.org/en-US/docs/Web/API/Window',
+                HTMLHeadingElement: 'https://developer.mozilla.org/en-US/docs/Web/API/HTMLHeadingElement',
+                Set: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set',
+                ClassMethodDecoratorContext: 'https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-0.html'
+            }
+            if (!(name in urls)) {
+                console.warn(`Can not find URL for typescript ${name} symbol`)
+            }
+            return urls[name]
+        },
         rxjs: ({ name }: { name: string }) => {
             const urls = {
                 Subject: 'https://www.learnrxjs.io/learn-rxjs/subjects/subject',
