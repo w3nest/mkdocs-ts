cde19b15e2ca35aa71fca6d884bd9bfd7ca6bf58
GReinisch <reinisch.gui@gmail.com>
2025-04-24 18:39:43 +0200

    💄 [doc-app] Display new loading screen

--- doc/src/app/main.ts
+++ doc/src/app/main.ts
@@ -1,25 +1,30 @@
 import './style.css'
 export {}
-import * as webpmClient from '@w3nest/webpm-client'
+import {install, LoadingScreen} from '@w3nest/webpm-client'
 
-import { setup } from '../auto-generated'
+import pkgJson from '../../package.json'
 import { DebugMode } from './config.debug'
 
 //eslint-disable-next-line @typescript-eslint/dot-notation
 window['mkdocsConfig'] = { enableContextual: DebugMode }
 
-await setup.installMainModule({
-    cdnClient: webpmClient,
-    installParameters: {
-        css: [
-            'bootstrap#5.3.3~bootstrap.min.css',
-            'fontawesome#5.12.1~css/all.min.css',
-            `mkdocs-ts#${setup.runTimeDependencies.externals['mkdocs-ts']}~assets/mkdocs-light.css`,
-            `mkdocs-ts#${setup.runTimeDependencies.externals['mkdocs-ts']}~assets/notebook.css`,
-            `mkdocs-ts#${setup.runTimeDependencies.externals['mkdocs-ts']}~assets/ts-typedoc.css`,
-        ],
-        displayLoadingScreen: true,
-    },
-})
+const version = pkgJson.webpm.dependencies['mkdocs-ts']
 
+const loadingScreen = new LoadingScreen({
+    name:pkgJson.name,
+    description:pkgJson.description,
+    logo:'../assets/favicon.svg'
+})
+await install({
+    esm:[`${pkgJson.name}#${pkgJson.version}`],
+    css: [
+        'bootstrap#5.3.3~bootstrap.min.css',
+        'fontawesome#5.12.1~css/all.min.css',
+        `mkdocs-ts#${version}~assets/mkdocs-light.css`,
+        `mkdocs-ts#${version}~assets/notebook.css`,
+        `mkdocs-ts#${version}~assets/ts-typedoc.css`,
+    ],
+    onEvent: (ev) => loadingScreen.next(ev)
+})
+loadingScreen.done()
 await import('./on-load')
