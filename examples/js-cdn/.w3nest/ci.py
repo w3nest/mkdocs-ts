1aa19597b418ee183877ef6cfd33384eeedbb654
GReinisch <reinisch.gui@gmail.com>
2025-09-12 12:10:46 +0200

    🔧 sync `ci.py` w/ new w3nest `js_app` module

--- examples/js-cdn/.w3nest/ci.py
+++ examples/js-cdn/.w3nest/ci.py
@@ -1,14 +1,29 @@
-from w3nest.app.projects import CIStep
-from w3nest.ci.js_app import PackageStep
+from w3nest.app.projects import Artifact, CIStep, FileListing
 
 # W3Nest client
 from w3nest_client import Context
 from w3nest.app.projects.models_ci import CI
 
+default_files = FileListing(
+    include=[
+        "*",
+        "*/**",
+    ],
+    ignore=[".w3nest", "node_modules"],
+    gitignore=True,
+)
+
 
 class SetupStep(CIStep):
     id: str = "setup"
     run: str = "yarn setup"
+
+
+class PackageStep(CIStep):
+    id: str = "package"
+    run: str = "echo 'Nothing to do'"
+    sources: FileListing = default_files
+    artifacts: list[Artifact] = [Artifact(id="package", files=default_files)]
 
 
 async def ci(context: Context) -> CI:
